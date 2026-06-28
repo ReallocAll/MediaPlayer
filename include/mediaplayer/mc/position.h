52fce795d40ecdb66d0dddeceda4f8783d95ea17
ReallocAll <63277109+ReallocAll@users.noreply.github.com>
2026-06-28 15:16:11 +0800

    feat: add actor_pos for tracking position changes

--- include/mediaplayer/mc/position.h
+++ include/mediaplayer/mc/position.h
@@ -4,6 +4,12 @@ struct vec3 {
 	float x, y, z;
 };
 
+struct actor_pos {
+    struct vec3 curr;    // +0
+    struct vec3 prev;    // +12
+    struct vec3 delta;   // +24
+};
+
 struct block_pos {
 	int x, y, z;
 };
